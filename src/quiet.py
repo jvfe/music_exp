e5a25016d2dfeedf2eb74236eaef982387f5cac0
jvfe <jvfecav@gmail.com>
2020-11-11 09:20:02 -0300

    Change order quiet

--- src/quiet.py
+++ src/quiet.py
@@ -24,11 +24,20 @@ p2 >> ambi(PxRand([2, 6, 8, 4, 12])[:10] + 0,
     mix=0.95
     )    
     
-s1 >> space(PRand([1, 4, 7, 9]) + 0,
-    dur=PRand(1,2)/4,
-    oct=3,
+rnd_amp6 = [PRand([0, 1])*.005*i for i in PRange(10)]    
+g1 >> glass(PxRand([2, 2, 4, 6, 6, 8, 9])[:10] + 0,
+    dur=PRand(4, 8)/2,
+    amp=rnd_amp6,
     sus=4,
-    amp=rnd_amp).every(2, "stutter").every(4, "shuffle")
+    room=.5)
+    
+
+rnd_amp3 = [PRand([0, 1])*.2*i for i in PRange(10)]
+k1 >> klank(PxRand([1, 4, 9, 7]) + 0,
+    dur=PRand(4,8)/2,
+    sus=4,
+    oct=4,
+    amp=rnd_amp3)
     
 
 b1 >> blip(PxRand([1, 4, 9, 7]) + 0,
@@ -36,13 +45,6 @@ b1 >> blip(PxRand([1, 4, 9, 7]) + 0,
     sus=4,
     amp=rndAmp2).every(2, "stutter")
     
-rnd_amp3 = [PRand([0, 1])*.2*i for i in PRange(10)]
-k1 >> klank(PxRand([1, 4, 9, 7]) + 0,
-    dur=PRand(4,8)/2,
-    pan=(1, -1),
-    sus=4,
-    oct=4,
-    amp=rnd_amp3)
     
 g1 >> gong(PRand([1, 4, 7, 9]) + 0,
     dur=PRand(4, 8)/2,
@@ -72,7 +74,12 @@ k3 >> bass([1, 3, 7, 5, PRand(10, 12)] * 2,
     room=.5,
     mix=0.5,
     lpr=.8).every(15, "shuffle")
-    
+
+s1 >> space(PRand([1, 4, 7, 9]) + 0,
+    dur=PRand(1,2)/4,
+    oct=3,
+    sus=4,
+    amp=rnd_amp).every(2, "stutter").every(4, "shuffle")
 
 rnd_amp5 = [PRand([0, 1])*.05*i for i in PRange(10)]    
 b2 >> bell(PwRand([2, 2, 4, 6, 6, 8, 9], [2, 4]) + 0,
@@ -95,15 +102,10 @@ p4 >> pluck(PxRand([2, 6, 8, 4, 12]) + 0,
     room=.3,
     dur=PRand(2, 4)/4).every(2, "stutter")
     
-rnd_amp6 = [PRand([0, 1])*.005*i for i in PRange(10)]    
 s4 >> space(PxRand([2, 2, 4, 6, 6, 8, 9]),
     amp=rnd_amp6,
     sus=4,
     room=.5)
     
-g1 >> glass(PxRand([2, 2, 4, 6, 6, 8, 9])[:10] + 0,
-    dur=PRand(4, 8)/2,
-    amp=rnd_amp6,
-    sus=4,
-    room=.5)
 
+Group(s1, s4).solo(0)
